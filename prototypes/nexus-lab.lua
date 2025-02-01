a71ca7688b450d4e9567fede736352bc478ed396
sZellwegerGit <s.zellweger@me.com>
2025-02-01 15:31:00 +0100

    Functional lab

--- prototypes/nexus-lab.lua
+++ prototypes/nexus-lab.lua
@@ -3,10 +3,10 @@ local sounds = require("__base__.prototypes.entity.sounds")
 local item = {
     type = "item",
     name = "nexus-lab",
-    icon = "__AncientKnowledge__/graphics/nexus-lab-icon.png",
+    icon = "__AncientKnowledge__/graphics/nexus-lab/nexus-lab-icon.png",
     icon_size = 64,
     subgroup = "production-machine",
-    order = "g[lab]",
+    order = "z[z-nexus-lab]",
     place_result = "nexus-lab",
     stack_size = 5,
     default_import_location = "nauvis",
@@ -16,7 +16,7 @@ local item = {
 local nexusLab = {
     type = "lab",
     name = "nexus-lab",
-    icon = "__AncientKnowledge__/graphics/nexus-lab-icon.png",
+    icon = "__AncientKnowledge__/graphics/nexus-lab/nexus-lab-icon.png",
     icon_size = 64,
     flags = {
         "placeable-neutral",
@@ -24,19 +24,15 @@ local nexusLab = {
         "player-creation"
     },
     minable = {
-        mining_time = 1.0,
+        mining_time = 0.5,
         result = "nexus-lab"
     },
     max_health = 1200,
     corpse = "big-remnants",
     dying_explosion = "medium-explosion",
     circuit_wire_max_distance = assembling_machine_circuit_wire_max_distance,
-    circuit_connector = circuit_connector_definitions["electromagnetic-plant"],
-    effect_receiver = { 
-        base_effect = {   
-            productivity = 0.25
-        }
-    },
+    circuit_connector = circuit_connector_definitions["biolab"],
+    heating_energy = "100kW",
     icon_draw_specification = {
         shift = {0, -0.6}, 
         scale = 1.8, 
@@ -57,7 +53,7 @@ local nexusLab = {
     working_sound = {
         sound = {
             {
-                filename = "__base__/sound/assembling-machine-t3-1.ogg",
+                filename = "__base__/sound/lab.ogg",
                 volume = 0.8
             },
             {
@@ -71,8 +67,8 @@ local nexusLab = {
         apparent_volume = 1.5,
     },
     collision_box = {
-        {-3.3, -3.3}, 
-        {3.3, 3.3}
+        {-3.2, -3.2}, 
+        {3.2, 3.2}
     },
     selection_box = {
         {-3.5, -3.5}, 
@@ -82,68 +78,93 @@ local nexusLab = {
         {-3.5, -3.5}, 
         {3.5, 3.5}
     },
-    graphics_set = {
-        animation = {
-            layers = {
-                {
-                  draw_as_shadow = true,
-                  filename = "__AncientKnowledge__/graphics/nexus-lab-shadow.png",
-                  priority = "high",
-                  width = 700,
-                  height = 600,
-                  frame_count = 1,
-                  line_length = 1,
-                  repeat_count = 99,
-                  animation_speed = 0.5,
-                  shift = util.by_pixel_hr(0, -16),
-                  scale = 0.5,
-                },
-                {
-                  priority = "high",
-                  width = 400,
-                  height = 400,
-                  frame_count = 99,
-                  shift = util.by_pixel_hr(0, -16),
-                  animation_speed = 0.5,
-                  scale = 0.5,
-                  stripes =
-                  {
-                    {
-                      filename = "__AncientKnowledge__/graphics/nexus-lab-animation.png",
-                      width_in_frames = 8,
-                      height_in_frames = 8,
-                    },
-                  },
-                },
-              },
-        },
-        working_visualisations = {
-            {
-                fadeout = true,
-                secondary_draw_order = 1,
-                animation = {
-                    layers = {
-                        {
-                            size = {400, 400},
-                            shift = util.by_pixel_hr(0, -16),
-                            scale = 0.5,
-                            frame_count = 99,
-                            draw_as_glow = true,
-                            blend_mode = "additive",
-                            animation_speed = 0.5,
-                            stripes = {
-                                {
-                                    filename = "__AncientKnowledge__/graphics/nexus-lab-emission.png",
-                                    width_in_frames = 8,
-                                    height_in_frames = 8,
-                                },
-                            },
-                        },
-                    },
-                },
-            }
-        },
-    },
+	on_animation = {
+		layers = {
+		
+		}
+	},
+	off_animation = {
+		layers = {
+			{
+			  draw_as_shadow = true,
+			  filename = "__AncientKnowledge__/graphics/nexus-lab/nexus-lab-shadow.png",
+			  priority = "high",
+			  width = 700,
+			  height = 600,
+			  frame_count = 1,
+			  line_length = 1,
+			  repeat_count = 1,
+			  animation_speed = 0.5,
+			  scale = 0.5,
+			},
+			{
+			  priority = "high",
+			  width = 400,
+			  height = 400,
+			  frame_count = 1,
+			  animation_speed = 0.5,
+			  scale = 0.5,
+			  stripes =
+			  {
+				{
+				  filename = "__AncientKnowledge__/graphics/nexus-lab/nexus-lab-animation.png",
+				  width_in_frames = 1,
+				  height_in_frames = 1,
+				}
+			  },
+			},
+		}
+	},
+	on_animation = {
+		layers = {
+			{
+			  draw_as_shadow = true,
+			  filename = "__AncientKnowledge__/graphics/nexus-lab/nexus-lab-shadow.png",
+			  priority = "high",
+			  width = 700,
+			  height = 600,
+			  frame_count = 1,
+			  line_length = 1,
+			  repeat_count = 60,
+			  animation_speed = 0.5,
+			  scale = 0.5,
+			},
+			{
+			  priority = "high",
+			  width = 400,
+			  height = 400,
+			  frame_count = 60,
+			  animation_speed = 0.5,
+			  scale = 0.5,
+			  stripes =
+			  {
+				{
+				  filename = "__AncientKnowledge__/graphics/nexus-lab/nexus-lab-animation.png",
+				  width_in_frames = 8,
+				  height_in_frames = 8,
+				}
+			  },
+			},
+			{
+			  priority = "high",
+			  width = 400,
+			  height = 400,
+			  frame_count = 60,
+			  animation_speed = 0.5,
+			  draw_as_glow = true,
+			  blend_mode = "additive",
+			  scale = 0.5,
+			  stripes =
+			  {
+				{
+				  filename = "__AncientKnowledge__/graphics/nexus-lab/nexus-lab-animation-emission.png",
+				  width_in_frames = 8,
+				  height_in_frames = 8,
+				}
+			  },
+			}
+		}
+	},
 	inputs = {
 		"automation-science-pack",
 		"logistic-science-pack",
@@ -159,9 +180,9 @@ local nexusLab = {
 		"promethium-science-pack",
     },
     researching_speed = 3,
-	science_pack_drain_rate_percent = 40,
+	science_pack_drain_rate_percent = 35,
 	allowed_effects = {"consumption", "speed", "productivity", "pollution"},
-	module_slots = 4,
+	module_slots = 5,
     energy_source = {
       type = "electric",
       usage_priority = "secondary-input",
